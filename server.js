e93e096e512934897f94a0296faf97c107256129
Pankaj Dhami <pandhami1@publicisgroupe.net>
2020-11-06 15:09:05 +0530

    changes

--- server.js
+++ server.js
@@ -1,50 +1,28 @@
+var express = require('express');
+var app = express();
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+ 
+app.get('/', function(req, res, next) { res.send('Hello world!'); });
+ 
+var server = app.listen(9000);
+ 
+var options = {
+    debug: true
+}
+ 
+app.use('/api', ExpressPeerServer(server, options));
+ 
 
-require('dotenv').config();
-const express = require("express");
-const http = require("http");
-const app = express();
-const server = http.createServer(app);
-const socket = require("socket.io");
-const io = socket(server);
-const path = require('path');
-
-const rooms = {};
-
-io.on("connection", socket => {
-    socket.on("join room", roomID => {
-        if (rooms[roomID]) {
-            rooms[roomID].push(socket.id);
-        } else {
-            rooms[roomID] = [socket.id];
-        }
-        const otherUser = rooms[roomID].find(id => id !== socket.id);
-        if (otherUser) {
-            socket.emit("other user", otherUser);
-            socket.to(otherUser).emit("user joined", socket.id);
-        }
-    });
-
-    socket.on("offer", payload => {
-        io.to(payload.target).emit("offer", payload);
-    });
-
-    socket.on("answer", payload => {
-        io.to(payload.target).emit("answer", payload);
-    });
-
-    socket.on("ice-candidate", incoming => {
-        io.to(incoming.target).emit("ice-candidate", incoming.candidate);
-    });
+server.on('connection', function(id) { //console.log(id) 
 });
 
+server.on('disconnect', function(id) { //console.log(id) 
+});
 
-    app.use(express.static(path.join(__dirname, './client/build')));
-    app.get('*', (req, res)=>{
-        res.sendFile(path.join(__dirname, './client/build/index.html'));
-
-    });
-
-
-
-const port = process.env.PORT || 80;
-server.listen(port, () => console.log(`server is running on port ${port}`));
+// // OR
+ 
+// var server = require('http').createServer(app);
+ 
+// app.use('/peerjs', ExpressPeerServer(server, options));
+ 
+// server.listen(9000);
