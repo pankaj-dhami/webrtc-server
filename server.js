5d75c1086cbc6af0d0160417eb78caeb7a1e3685
Pankaj Dhami <pandhami1@publicisgroupe.net>
2020-11-06 16:58:28 +0530

    change in code

--- server.js
+++ server.js
@@ -4,25 +4,27 @@ var ExpressPeerServer = require('peer').ExpressPeerServer;
  
 app.get('/', function(req, res, next) { res.send('Hello world!'); });
  
-var server = app.listen(80);
+// var server = app.listen(80);
  
 var options = {
     debug: true
 }
  
-app.use('/api', ExpressPeerServer(server, options));
+// app.use('/api', ExpressPeerServer(server, options));
  
 
-server.on('connection', function(id) { //console.log(id) 
-});
 
-server.on('disconnect', function(id) { //console.log(id) 
-});
 
 // // OR
  
-// var server = require('http').createServer(app);
+ var server = require('http').createServer(app);
  
-// app.use('/peerjs', ExpressPeerServer(server, options));
+ app.use('/peerjs', ExpressPeerServer(server, options));
  
-// server.listen(9000);
+ server.listen(80);
+
+ server.on('connection', function(id) { //console.log(id) 
+ });
+ 
+ server.on('disconnect', function(id) { //console.log(id) 
+ });
